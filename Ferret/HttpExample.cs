8fcfb7066810ad8c926c757c95904b0543eb9a10
Simon Moreau <simon@bim42.com>
2022-03-03 11:16:47 +0100

    setup a new version of Ferret

---
+++ Ferret/HttpExample.cs
@@ -0,0 +1,53 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Azure.Cosmos.Table;
+
+namespace Ferret
+{
+    public static class HttpExample
+    {
+        [FunctionName("HttpExample")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            [Table("entities")] IAsyncCollector<TestEntity> entitiesTable,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            TestEntity token = new TestEntity();
+            token.ForgeToken = name;
+
+            await entitiesTable.AddAsync(token);
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+
+    public class TestEntity : TableEntity
+    {
+        public TestEntity()
+        {
+            this.RowKey = "token";
+            this.PartitionKey = "token";
+            this.ETag = "*";
+        }
+        public string ForgeToken { get; set; }
+    }
+}
